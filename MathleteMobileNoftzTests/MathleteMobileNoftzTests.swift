7f2bb6670411bf162328bc3cb21b4fb6cd13893e
Evangeline Noftz <esnoftz@gmail.com>
2024-10-28 08:45:26 -0500

    Initial Commit

---
+++ MathleteMobileNoftzTests/MathleteMobileNoftzTests.swift
@@ -0,0 +1,17 @@
+//
+//  MathleteMobileNoftzTests.swift
+//  MathleteMobileNoftzTests
+//
+//  Created by EVANGELINE NOFTZ on 10/28/24.
+//
+
+import Testing
+@testable import MathleteMobileNoftz
+
+struct MathleteMobileNoftzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
