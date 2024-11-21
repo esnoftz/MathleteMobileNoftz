0c86c5bbeb59992116b62c8e9440d7e9f5f71588
Evangeline Noftz <esnoftz@gmail.com>
2024-11-21 08:06:20 -0600

    Worked on student profile.

--- MathleteMobileNoftz/Student.swift
+++ MathleteMobileNoftz/Student.swift
@@ -10,9 +10,18 @@ import Foundation
 class Student: Codable {
     var name: String
     var grade: Int
-    
-    init(name: String, grade: Int) {
+    var mathCourses = [String]()
+    var testScores = [Int]()
+    var awards = [String]()
+    var compsAttended = [String]()
+    //var picture
+
+    init(name: String, grade: Int, mathCourses: [String] = [String](), testScores: [Int] = [Int](), awards: [String] = [String](), compsAttended: [String] = [String]()) {
         self.name = name
         self.grade = grade
+        self.mathCourses = mathCourses
+        self.testScores = testScores
+        self.awards = awards
+        self.compsAttended = compsAttended
     }
 }
