c78584fb7e3c41c0a5938bdecbf1792cc196ef80
Evangeline Noftz <esnoftz@gmail.com>
2024-12-12 08:07:42 -0600

    Worked on splitting table view.

---
+++ MathleteMobileNoftz/FSAddedCell.swift
@@ -0,0 +1,17 @@
+//
+//  FSAddedCell.swift
+//  MathleteMobileNoftz
+//
+//  Created by EVANGELINE NOFTZ on 12/12/24.
+//
+
+import Foundation
+import UIKit
+
+class FSAddedCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel2: UILabel!
+    
+    @IBOutlet weak var gradeLabel2: UILabel!
+    
+}
